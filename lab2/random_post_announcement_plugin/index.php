ff0d56330eed9f4f409aefb7aa7c9e4cea68039f
Jakub Czajkowski <and.jj.an@gmail.com>
2025-03-17 10:16:56 +0100

    feat: add date range picker

--- lab2/random_post_announcement_plugin/index.php
+++ lab2/random_post_announcement_plugin/index.php
@@ -14,19 +14,35 @@
 
  function rpa_admin_page() {
     if (isset($_POST['rpa_save_announcements'])) {
-        $announcements = array_filter($_POST['rpa_announcements']);
+        $announcements = [];
+        foreach ($_POST['rpa_announcements'] as $key => $announcement) {
+            // Check if both announcement text and dates are provided
+            $start_date = isset($_POST['rpa_start_date'][$key]) ? $_POST['rpa_start_date'][$key] : '';
+            $end_date = isset($_POST['rpa_end_date'][$key]) ? $_POST['rpa_end_date'][$key] : '';
+            if (!empty($announcement)) {
+                // Save as an array of announcement data
+                $announcements[] = [
+                    'announcement' => $announcement,
+                    'start_date' => $start_date,
+                    'end_date' => $end_date
+                ];
+            }
+        }
         update_option('rpa_announcements', $announcements);
         echo '<div class="updated"><p>Announcements saved.</p></div>';
     }
+
     $announcements = get_option('rpa_announcements', []);
     ?>
     <div class="wrap">
         <h1>Random Post Announcements</h1>
         <form method="post">
             <div id="announcements-container">
-                <?php foreach ($announcements as $announcement) : ?>
+                <?php foreach ($announcements as $index => $announcement) : ?>
                     <div class="announcement-item">
-                        <input type="text" name="rpa_announcements[]" value="<?php echo esc_attr($announcement); ?>" style="width:80%;">
+                        <input type="text" name="rpa_announcements[]" value="<?php echo esc_attr($announcement['announcement']); ?>" style="width:80%;">
+                        <input type="date" name="rpa_start_date[]" value="<?php echo esc_attr($announcement['start_date']); ?>">
+                        <input type="date" name="rpa_end_date[]" value="<?php echo esc_attr($announcement['end_date']); ?>">
                         <button type="button" class="remove-announcement">Remove</button>
                     </div>
                 <?php endforeach; ?>
@@ -43,6 +59,8 @@
                 let newInput = document.createElement('div');
                 newInput.classList.add('announcement-item');
                 newInput.innerHTML = '<input type="text" name="rpa_announcements[]" style="width:80%;" placeholder="New Announcement">' +
+                                     '<input type="date" name="rpa_start_date[]" />' +
+                                     '<input type="date" name="rpa_end_date[]" />' +
                                      '<button type="button" class="remove-announcement">Remove</button>';
                 container.appendChild(newInput);
             });
@@ -61,8 +79,22 @@ function rpa_add_announcement($content) {
     if (is_single()) {
         $announcements = get_option('rpa_announcements', []);
         if (!empty($announcements)) {
-            $announcement = $announcements[array_rand($announcements)];
-            $content = '<div class="rpa-announcement">' . wp_kses_post($announcement) . '</div>' . $content;
+            $today = date('Y-m-d');
+            $valid_announcements = [];
+
+            // Filter announcements based on current date
+            foreach ($announcements as $announcement) {
+                if ((empty($announcement['start_date']) || $today >= $announcement['start_date']) &&
+                    (empty($announcement['end_date']) || $today <= $announcement['end_date'])) {
+                    $valid_announcements[] = $announcement['announcement'];
+                }
+            }
+
+            // Display a random valid announcement if any
+            if (!empty($valid_announcements)) {
+                $announcement = $valid_announcements[array_rand($valid_announcements)];
+                $content = '<div class="rpa-announcement">' . wp_kses_post($announcement) . '</div>' . $content;
+            }
         }
     }
     return $content;
